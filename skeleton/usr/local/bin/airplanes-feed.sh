ad11d31453dfde0971d3722f4855c85865523955
SammySkycrafts <77464976+SammySkycrafts@users.noreply.github.com>
2023-09-18 00:16:16 +0100

    Set fallback feed url

--- skeleton/usr/local/bin/airplanes-feed.sh
+++ skeleton/usr/local/bin/airplanes-feed.sh
@@ -5,10 +5,9 @@ do
     /usr/bin/airplanes-feeder --quiet --net --net-only \
         --db-file=none --max-range 450 \
         --net-beast-reduce-interval 0.5 \
-        --net-connector feed.airplanes.live,30004,beast_reduce_plus_out,feed.airplanes.live,64004 \
+        --net-connector feed.airplanes.live,30004,beast_reduce_plus_out,feed2.airplanes.live,64004 \
         --net-connector 127.0.0.1,30005,beast_in \
-        --net-ro-interval 0.2 --net-ri-port 0 --net-ro-port 0 \
-        --net-sbs-port 0 --net-bi-port 0 --net-bo-port 0 \
+        --net-ro-interval 0.2 \
         --json-location-accuracy 2 --write-json /run/airplanes-feed \
         --lat $LATITUDE --lon $LONGITUDE
 done
