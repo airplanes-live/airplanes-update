00d5a3fa433b2477da05b2eba56299f07ab1d789
ADS-B related code <34561158+adsb-related-code@users.noreply.github.com>
2025-02-11 18:11:45 -0700

    Update resetdefaults.sh
    remove wpa_supplicant

--- resetdefaults.sh
+++ resetdefaults.sh
@@ -13,20 +13,12 @@ apt clean -y
 echo -e "\n\n RESET UUID"
 rm -f /boot/airplanes-uuid
 
-
-#ZT removed May 12th - RA
-#echo -e "\n\n RESET ZT"
-#rm -f /var/lib/zerotier-one/identity.*
-#rm -f /var/lib/zerotier-one/authtoken.secret
-
 echo -e "\n RESET SSH"
 rm /etc/ssh/ssh_host_*
 
 echo -e "\n REMOVE BASH HISTORY"
 rm /home/pi/.bash_history
 
-
-# fail if this directory doesn't exist
 if ! [[ -d /airplanes/update/boot-configs ]]; then
     exit 1
 fi
@@ -37,9 +29,5 @@ for file in *; do
     cp --remove-destination -f -T "$file" "/boot/$file"
 done
 popd &>/dev/null
-
-echo -e "\n RESET WPA_SUPPLICANT CONF"
-rm -f /etc/wpa_supplicant/wpa_supplicant.conf
-
 
 exit 0
