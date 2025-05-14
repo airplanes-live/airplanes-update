e0dac768c57e369bf7c449b4d1b49c7b12c22ba2
ADS-B related code <34561158+adsb-related-code@users.noreply.github.com>
2025-05-14 10:10:27 -0700

    Update readsb.sh
    gain auto moved to list

--- skeleton/usr/local/bin/readsb.sh
+++ skeleton/usr/local/bin/readsb.sh
@@ -8,19 +8,11 @@ if [[ "$MODEAC" == "yes" ]]; then
     DECODER_OPTIONS=$DECODER_OPTIONS" --modeac"
 fi
 
-if [[ "$AUTOGAIN" == "yes" ]]; then
-    $GAIN_OPT=" --gain=auto"
-else
-    $GAIN_OPT=" --gain=$GAIN"
-fi
-
-
-
 exec /usr/bin/readsb \
     --net-api-port 30152 \
     --net-json-port 30154 \
     --write-prom /run/readsb/stats.prom \
-     $GAIN_OPT \
+     --gain $GAIN \
     --lat $LATITUDE \
     --lon $LONGITUDE \
     $RECEIVER_OPTIONS \
